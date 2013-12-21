c75259309e71f793cb7757ae40bc90bb2ad56fb5
Yen <cyu2197@gmail.com>
2013-12-22 00:02:14 +0800

    add function: slide autoplay with spped control

--- js/postMsgHandle.js
+++ js/postMsgHandle.js
@@ -1,10 +1,15 @@
 window.addEventListener('message', function(event){
 	var req = JSON.parse(event.data);
+	
 	if(req.cmd == 'get_total'){
 		var num = document.querySelectorAll( '.reveal .slides>section' ).length;
 		window.parent.postMessage(JSON.stringify({'cmd':'get_total', 'value':num}),'*');
+		
 	}else if(req.cmd == 'jump_to'){
 		Reveal.slide(req.value);
+		
+	}else if(req.cmd == 'autoplay'){
+		Reveal.configure({ autoSlide: req.value });
 	}
 });
 
